2377963dfd7450889f7facb23da643513741d573
RJ Boeke <rjboeke@microsoft.com>
2016-04-17 23:15:02 -0700

    source mappings added

---
+++ isomorphic_A.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="isomorphic_A/typings/isomorphic_A/typings/isomorphic_A/typings/node/node.d.ts" />
+/// <reference path="isomorphic_A/typings/isomorphic_A/typings/react/react.d.ts" />
+/// <reference path="isomorphic_A/typings/react/react-dom.d.ts" />
--- isomorphic_A/webpack.config.js
+++ isomorphic_A/webpack.config.js
@@ -7,11 +7,38 @@ module.exports = {
   ],
   output: {
     path:     path.join(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    sourcemaps: '[file].js.map'
   },
+  devtool: 'source-map',
+  plugins: [
+      new webpack.SourceMapDevToolPlugin({
+          test:  /\.tsx?$/,
+          exclude: 'node_modules',
+          module: true,
+          filename: '[file].map',
+          append: false
+      })
+  ],
   resolve: {
     modulesDirectories: ['node_modules'],
     extensions:         ['', '.js', '.jsx', '.ts', '.tsx']
+  },
+  ts: {
+      "compilerOptions": {
+        "target": "es5",
+        "sourceMap": true,
+        "module": "commonjs",
+        "moduleResolution": "node",
+        "jsx": "react",
+        "allowSyntheticDefaultImports": true,
+        "noLib": false,
+        "declaration": false,
+        "noImplicitAny": true
+      },
+      "exclude": [
+        "node_modules"
+      ]
   },
   module: {
     loaders: [
