d561d1a598825698dee46459d826efaf3addebfc
RJ Boeke <rjboeke@microsoft.com>
2016-04-20 15:44:15 -0700

    initial check in - even more changes to readme.txt

--- isomorphic_A.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="isomorphic_A/typings/isomorphic_A/typings/isomorphic_A/typings/node/node.d.ts" />
-/// <reference path="isomorphic_A/typings/isomorphic_A/typings/react/react.d.ts" />
-/// <reference path="isomorphic_A/typings/react/react-dom.d.ts" />
